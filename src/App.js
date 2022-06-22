63dbb7611f19e1ace33a2cee27d506b69a200c78
Paul Connell <paulconnell@fastmail.fm>
2022-06-22 09:27:30 +0100

    Changes to code after pull review.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import SearchBar from './SearchBar';
 import GenreToggle from './GenreToggle';
 
 function App() {
-  let genres = ["All", "Documentary", "Comedy", "Horror", "Crime"];
+const genres = ["All", "Documentary", "Comedy", "Horror", "Crime"];
   return (
     <div className="App">
       {React.createElement("div", {}, "Hello World!")}
@@ -14,7 +14,7 @@ function App() {
       <br />
       <SearchBar />
       <br />
-      <GenreToggle genres={genres} onGenreSelected={(genre) => { alert(genre) }} />
+      <GenreToggle genres={genres} onGenreSelected={(genre) => { console.log(genre) }} />
     </div>
   );
 }
--- src/GenreToggle.jsx
+++ src/GenreToggle.jsx
@@ -1,34 +1,30 @@
 /** Genre Toggle Component 
- * Using PureComponent to stop renrendering on state in parent changing potentially (https://betterprogramming.pub/when-to-use-react-purecomponent-723f85738be1)
+ * Using PureComponent to stop re-rendering on state in parent changing potentially (https://betterprogramming.pub/when-to-use-react-purecomponent-723f85738be1)
 */
 
 import React from "react";
 
 export default class GenreToggle extends React.PureComponent {
-    constructor(props) {
-        super(props);
-        this.state = {
-            chosenGenre: "All" // default
-        };
-    }
+    state = {
+        chosenGenre: "All" // default
+    };
 
     handleChange = genre => {
         this.setState({ chosenGenre: genre });
         this.props.onGenreSelected(genre);
     }
 
-    genreItems = () => {
-        return this.props.genres.map(genre => {
-            return (<div key={genre} className={this.state.chosenGenre === genre ? "genreItem selected" : "genreItem"}>
-                <span onClick={() => { this.handleChange(genre); }}>{genre}</span>
+    renderItems = (genres) => genres.map(genre => {
+            let setClass = this.state.chosenGenre === genre ? "genreItem selected" : "genreItem";
+            return (<div key={genre} className={setClass}>
+                <span onClick={() => this.handleChange(genre)}>{genre}</span>
             </div>);
         });
-    }
 
     render = () => {
         return (
             <div className="genreChoices">
-                {this.genreItems()}
+                {this.renderItems(this.props.genres)}
             </div>
         )
     }
