b2d488227cd3ec87d193a1055c002bb69a909b6c
Paul Connell <paulconnell@fastmail.fm>
2022-07-14 15:41:37 +0100

    Removed css compile, using styled-components library.

--- webpack.common.js
+++ webpack.common.js
@@ -8,10 +8,6 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
-            {
                 test: /\.?js|\.?jsx$/,
                 exclude: /node_modules/,
                 use: {
