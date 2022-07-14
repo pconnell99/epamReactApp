3c8558f8587e7fba05580c8494fb21163980c98b
Paul Connell <paulconnell@fastmail.fm>
2022-07-14 15:41:50 +0100

    Serving images from /public/images

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,10 +1,14 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+const path = require('path');
 
 module.exports = merge(common, {
-    mode: 'development',
-    devtool: 'inline-source-map',
-    devServer: {
-        static: 'temp',
-    },
+	mode: "development",
+	devtool: "inline-source-map",
+	devServer: {
+		static: {
+			directory: path.resolve(__dirname, "./public/images"),
+			publicPath: "/images",
+		},
+	},
 });
